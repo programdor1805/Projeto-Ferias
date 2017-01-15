a2365c5d17982c2ff5d522a8cecde33f596a5005
programdor1805 <tiago_alves1805@hotmail.com>
2017-01-14 23:40:37 -0200

    Add files via upload

---
+++ PedidoVenda/src/main/java/br/com/tiago/controller/CadastroPedidoBean.java
@@ -0,0 +1,25 @@
+package br.com.tiago.controller;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class CadastroPedidoBean {
+
+		private List<Integer> itens;
+
+		public CadastroPedidoBean(){
+			itens = new ArrayList<>();
+			itens.add(1);
+		}
+		
+		public List<Integer> getItens(){
+			return itens;
+		}
+}
+
